493070c85bf82f3053e8e9e96691e109b3db7aad
Thales De Souza Machado <thalesm@id.uff.br>
2019-06-27 11:54:20 -0300

    criacao do outline das funções do façade

--- br/uff/ps/Aplicacao.java
+++ br/uff/ps/Aplicacao.java
@@ -1,4 +1,16 @@
 
 public class Aplicacao {
-	public void ComprarPassagem
+	public void comprarPassagemAerea(){}
+
+	public void comprarPasseioTuristico(){}
+
+	public void comprarReserva(){}
+
+	public List<String> getReservasCompradas(){}
+
+	public List<String> getPasseiosCOmprados(){}
+
+	public List<String> getPassagensAereas(){}
+
+	public List<String> getTodasCompras(){}
 }
