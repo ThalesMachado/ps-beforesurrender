0ee9e02d314b6dd004c8201974aeab812f4356f6
ThalesMachado <thalesmid.uff.br>
2019-06-27 03:19:09 +0000

    CRiação de novas classes

---
+++ br/uff/ps/PassagemAerea.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class PassagemAerea {
+	private String numeroPassagem;
+	private String empresArea;
+	private int quantidadeBagagem;
+	private ArrayList<String> classePassagem;
+	private ArrayList<String> assentos;
+	Voo voo;
+	
+	
+	public void escolherAssentos(ArrayList<String> assentos) {
+		for (int i = 0 ; i < passagens.size() ; i++) {
+			this.assentos.add(assentos.get(i));
+		}
+	}
+}
---
+++ br/uff/ps/Voo.java
@@ -0,0 +1,12 @@
+
+public class Voo {
+	private String idVoo;
+	private int aeronave;
+	private String siglaAeroportoOrigem;
+	private String siglaAeroportoDestino;
+	private DateTime dataSaida;
+	private DateTime dataChegada;
+	private boolean cancelado = false;
+	private CompanhiaAerea companhia;
+	
+}
